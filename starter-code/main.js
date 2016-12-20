4e5fecae80ccf24ff5291d885b6754b6cc31497d
Valerio Risuleo <valeriorisuleo@valerios-mbp-001.homenet.telecomitalia.it>
2016-12-20 22:25:23 +0100

    created game logica

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "King";
 var cardFour = "King";
+var areBothEven = function(cardTwo, cardFour) {
+	if (cardTwo % 2 === 0 && cardFour === 0) {
+		alert("you find a match");
+	}
+	else{
+		alert("Sorry");
+	}
+};
