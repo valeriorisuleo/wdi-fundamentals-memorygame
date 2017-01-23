6ec6ae3a9131798a9fea9668e6c49b56847425b4
Valerio Risuleo <valeriorisuleo@Valerios-MacBook-Pro.local>
2017-01-23 01:21:40 +0000

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,60 +1,42 @@
 var cards = ['queen', 'queen', 'king', 'king'];
+
 var cardsInPlay = [];
 
 
 
 var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', isTwoCards);
 
+    board.appendChild(newCard);
+  }
 
-function createCards() {
-	for(var i=0; i<cards.length; i++) {
-		//create element
-		var newCard = document.createElement('div');
-		//assign class
-		newCard.className = 'card';
-		//attach it to the board
-		document.getElementsByTagName('div')[0].appendChild(newCard);
-
-		newCard.setAttribute('data-card', cards[i]);
-		
-		// when a card is clicked the function isTwoCards will be executed
-		newCard.addEventListener('click', isTwoCards);
-	}
-};
-
-function isMatch(cards) {
-	// body...
-	if (card[0]==card[1]) {
-		alert("We got a match!")
-	} else {
-		alert("We are sorry!")
-	}
 }
-
-function isTwoCards(cards) {
-	// body...
-	cardsInPlay.push(this.getAttribute('data-card'));
-	// show the card's image
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
 	console.log(this.getAttribute('data-card'));
 	if (this.getAttribute('data-card') === 'king') {
 		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
 	} else {
 		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
 	}
-	if (cardsInPlay.length == 2) {
-
-		// if you have two cards in play check for a match
-		isMatch(cardsInPlay);
-
-		// clear cards in play array for next try
-    	cardsInPlay = [];
-	}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
 }
 
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
 
-createCards();
+  }
+}
 
-
-
-
-
+createBoard();
